4bfe9208bfe7200c93535a420547b47352ea5192
dicksonace <dicksonawudu362375@gmail.com>
2022-10-19 01:39:43 +0000

    hero

--- v1/src/components/Hero.jsx
+++ v1/src/components/Hero.jsx
@@ -26,6 +26,29 @@ const Hero = () => {
             Search
           </button>
         </div>
+
+        <div className="w-100 flex flex-row justify-between">
+          <div>
+            <h1 className="text-4xl text-white font-bold border-b border-blue-400 py-5">232</h1>
+            <p className="font-bold py-2 text-white">Clients</p>
+          </div>
+
+          <div>
+            <h1 className="text-4xl text-white font-bold border-b border-blue-400 py-5">521</h1>
+            <p className="font-bold py-2 text-white">Projects</p>
+          </div>
+
+
+          <div>
+            <h1 className="text-4xl text-white font-bold border-b border-blue-400 py-5">1453</h1>
+            <p className="font-bold py-2 text-white">Support</p>
+          </div>
+
+          <div>
+            <h1 className="text-4xl text-white font-bold border-b border-blue-400 py-5">32</h1>
+            <p className="font-bold py-2 text-white">Workers</p>
+          </div>
+        </div>
       </div>
       <div className="w-30">
         <img className="md:w-[1700px] md:block hidden" src={Hero1} alt="/" />
